cc14c5535b7b5ba23ca9c20d9bf8279697ce1798
miguelba18 <miguel_bahamonro@fet.edu.co>
2026-03-11 09:16:15 -0500

    style: agregar logs para ver los endpoints que ejecuta el backend

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,41 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+process.removeAllListeners('warning');
+process.on('warning', (warning) => {
+  if (warning.name === 'DeprecationWarning' && warning.message.includes('client.query')) return;
+  console.warn(warning);
+});
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,
+    {
+      logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+    },
+  );
+const logger = new Logger('HTTP');
 
+  app.use((req: any, res: any, next: any) => {
+    const { method, originalUrl } = req;
+    const start = Date.now();
+
+    res.on('finish', () => {
+      const duration = Date.now() - start;
+      const statusCode = res.statusCode;
+
+      const color =
+        statusCode >= 500 ? '🔴' :
+        statusCode >= 400 ? '🟡' :
+        statusCode >= 300 ? '🔵' :
+        '🟢';
+
+      logger.log(
+        `${color} ${method} ${originalUrl} → ${statusCode} (${duration}ms)`
+      );
+    });
+
+    next();
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -22,5 +53,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`App running on port ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
